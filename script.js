88f3f8b74cf4ad4a34b7791f48314b7df13a48aa
itsmyrice <gaconwi@googlemail.com>
2024-03-01 16:15:08 +0100

    add responsive design

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const slideshow = () => {
 
   console.log(slides);
 
-  if (current > slides.length) {
+  if(current > slides.length) {
     current = 1;
   } else if (current === 0) {
     current = slides.length;
   }
 
-  slides.forEach((slide) => {
-    if (slide.classList[1].split("-")[1] * 1 === current) {
+  slides.forEach(slide => {
+    if(slide.classList[1].split("-")[1] * 1 === current) {
       slide.style.cssText = "visibility: visible; opacity: 1;";
     } else {
       slide.style.cssText = "visibility: hidden; opacity; 0;";
