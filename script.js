a320e4ac1673f5e2a4acd3cf9e3a84d94456cd7d
itsmyrice <gaconwi@googlemail.com>
2024-03-01 14:29:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let current = 1;
+
+const slideshow = () => {
+  const slides = Array.from(document.querySelectorAll(".slide"));
+
+  console.log(slides);
+
+  if (current > slides.length) {
+    current = 1;
+  } else if (current === 0) {
+    current = slides.length;
+  }
+
+  slides.forEach((slide) => {
+    if (slide.classList[1].split("-")[1] * 1 === current) {
+      slide.style.cssText = "visibility: visible; opacity: 1;";
+    } else {
+      slide.style.cssText = "visibility: hidden; opacity; 0;";
+    }
+  });
+  current++;
+};
+
+slideshow();
+
+setInterval(() => {
+  slideshow();
+}, 4000);
+
+const sidebar = document.querySelector(".sidebar");
+const sidebarButton = document.querySelector(".sidebar-button");
+const menuIcon = document.querySelector(".menu-icon");
+const closeButton = document.querySelector(".sidebar i");
+
+menuIcon.addEventListener("click", () => {
+  sidebar.classList.add("navigate");
+});
+
+closeButton.addEventListener("click", () => {
+  sidebar.classList.remove("navigate");
+});
+
+sidebarButton.addEventListener("click", () => {
+  sidebar.classList.toggle("change");
+});
